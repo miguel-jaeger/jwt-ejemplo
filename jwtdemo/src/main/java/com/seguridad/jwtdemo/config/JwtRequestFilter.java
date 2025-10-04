c6b80b5a417eb5b259dadff0c107af633dbb6718
miguel-jaeger <84285594+miguel-jaeger@users.noreply.github.com>
2025-10-04 13:57:36 -0500

    Update JwtRequestFilter.java

--- jwtdemo/src/main/java/com/seguridad/jwtdemo/config/JwtRequestFilter.java
+++ jwtdemo/src/main/java/com/seguridad/jwtdemo/config/JwtRequestFilter.java
@@ -59,7 +59,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         username, null, authorities);
                 
-                // 🔑 6. Establecer el contexto de seguridad (¡Esto autentica la petición!)
+                // 6. Establecer el contexto de seguridad (¡Esto autentica la petición!)
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
